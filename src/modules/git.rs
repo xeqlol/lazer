9f08a4d411accc4599d5a5bbde13c8c0c53d5cd2
Dimitrii Nemkov <xeqlol@gmail.com>
2025-03-09 17:34:58 +0500

    chore: minor changes

--- src/modules/git.rs
+++ src/modules/git.rs
@@ -5,6 +5,24 @@ use crate::segment::Segment;
 
 const DEFAULT_TEMPLATE: &str = r"\[git [$branch$is_dirty](f:blue b)\]";
 
+fn has_uncommitted_changes(repo: &Repository) -> Result<bool, git2::Error> {
+    let statuses = repo.statuses(None)?;
+
+    for entry in statuses.iter() {
+        let status = entry.status();
+        if status.intersects(
+            git2::Status::WT_MODIFIED
+                | git2::Status::WT_NEW
+                | git2::Status::WT_DELETED
+                | git2::Status::WT_RENAMED
+                | git2::Status::WT_TYPECHANGE,
+        ) {
+            return Ok(true);
+        }
+    }
+    Ok(false)
+}
+
 pub fn get_git_segment<'a>() -> Option<Segment<'a>> {
     let repo = Repository::open(env::current_dir().unwrap());
 
@@ -24,7 +42,7 @@ pub fn get_git_segment<'a>() -> Option<Segment<'a>> {
     let head = head.as_ref().and_then(|h| h.shorthand());
 
     let branch: &str = head.unwrap_or("HEAD");
-    let is_dirty = !repo.statuses(None).unwrap().is_empty();
+    let is_dirty = has_uncommitted_changes(&repo).unwrap();
 
     let mut variables = HashMap::new();
     variables.insert("branch", branch.to_string());
