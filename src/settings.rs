06ea6fe2bbbb5b85d4464e4b96a3935dd1439af3
Dimitrii Nemkov <xeqlol@gmail.com>
2025-03-18 14:53:22 +0500

    chore: minor changes

--- src/settings.rs
+++ src/settings.rs
@@ -1,3 +1,5 @@
+use std::{fmt::format, path::Path};
+
 use config::{Config, ConfigError, File};
 use serde_derive::Deserialize;
 
@@ -12,8 +14,11 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
+        let config_dir = dirs::config_dir().unwrap();
+        let config_dir = config_dir.to_str();
+        let config_file = format!("{}/lazer.toml", config_dir.unwrap());
         let settings = Config::builder()
-            .add_source(File::with_name("/Users/d.nemkov/Fun/lazer/lazer.toml"))
+            .add_source(File::with_name(&config_file))
             .build()?;
 
         settings.try_deserialize()
